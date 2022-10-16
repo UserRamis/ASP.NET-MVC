47336f44b9c52bf43cd01fcd69f546da510717ce
Ramis Garipov <ramis2003@mail.ru>
2022-10-16 15:54:19 +0300

    Added the first part of adding categories

--- building_materials_store/Models/Category.cs
+++ building_materials_store/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace building_materials_store.Models
 {
@@ -9,7 +10,7 @@ namespace building_materials_store.Models
         public int Id { get; set; }
 
         public string Name { get; set; }
-
+        [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
 
 
